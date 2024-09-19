e2eae3277c23773b40df5e124d94979df8be4461
Gseungjin2 <jchsekfr5@gmail.com>
2024-09-19 20:00:59 +0900

    REFACTOR : ACCOUNT 수정/ account_number_generator.py 에러핸들러 추가 costs,py 상수 사용하여 계좌번호 길이 정의

--- src/accounts/services/account_number_generator.py
+++ src/accounts/services/account_number_generator.py
@@ -2,15 +2,18 @@ import random
 import string
 from django.core.exceptions import ValidationError
 from accounts.models import Account
-from accounts.services.consts import BANK_PREFIXES, MAX_ATTEMPTS
+from accounts.services.consts import BANK_PREFIXES, MAX_ATTEMPTS, ACCOUNT_NUMBER_LENGTH
 
 
 class AccountNumberGenerator:
-    @classmethod
-    def generate(cls, bank_code):
-        prefix = BANK_PREFIXES.get(bank_code, 'UK')
+    @staticmethod
+    def generate(bank_code):
+        if bank_code not in BANK_PREFIXES:
+            raise ValidationError(f"유효하지 않은 은행 코드입니다: {bank_code}")
+
+        prefix = BANK_PREFIXES[bank_code]
         for _ in range(MAX_ATTEMPTS):
-            number = ''.join(random.choices(string.digits, k=13))
+            number = ''.join(random.choices(string.digits, k=ACCOUNT_NUMBER_LENGTH))
             account_number = f"{prefix}_{number}"
             if not Account.objects.filter(account_number=account_number).exists():
                 return account_number
